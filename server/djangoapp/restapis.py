301bfc00eaf107dd86d5edf1d344defb26d948a4
nieves-mon <cunanan.nm@gmail.com>
2024-02-20 14:55:20 -0500

    wip

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from .models import CarDealer
+from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
 
 
@@ -10,7 +10,7 @@ def get_request(url, **kwargs):
 	print("GET from {} ".format(url))
 	try:
 		# Call get method of requests library with URL and parameters
-		response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs)
+		response = requests.get(url, params=params, headers={'Content-Type': 'application/json'})
 		status_code = response.status_code
 		print("With status {} ".format(status_code))
 		json_data = json.loads(response.text)
@@ -51,15 +51,42 @@ def get_dealers_from_cf(url, **kwargs):
 
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
-# def get_dealer_by_id_from_cf(url, dealerId):
-# - Call get_request() with specified arguments
-# - Parse JSON results into a DealerView object list
+def get_dealer_reviews_from_cf(url, dealerId):
+	results = []
+	# Call get_request with a URL parameter
+	reviews = get_request(url)
 
+	if reviews:
+		# For each dealer object
+		for review_doc in reviews:
+			if review_doc["purchase"]:
+				review_obj = DealerReview(
+					dealership=review_doc["dealership"],
+					name=review_doc["name"],
+					purchase=review_doc["purchase"],
+					review=review_doc["review"],
+					purchase_date=review_doc["purchase_date"],
+					car_make=review_doc["car_make"],
+					car_model=review_doc["car_model"],
+					car_year=review_doc["car_year"],
+					sentiment=analyze_review_sentiments(review_doc["review"]),
+					id=review_doc["id"],
+				)
+				results.append(review_obj)
+	return results
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
-# - Call get_request() with specified arguments
-# - Get the returned sentiment label such as Positive or Negative
+def analyze_review_sentiments(text):
+	url = 'https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/7250d85b-153d-4cec-a2de-8014ff4e59f2'
 
+	params = dict()
+	params["text"] = kwargs["text"]
+	params["version"] = kwargs["version"]
+	params["features"] = kwargs["features"]
+	params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+	response = requests.get(url, params=params, headers={'Content-Type': 'application/json'}, auth=HTTPBasicAuth('apikey', api_key))
 
-
+	try:
+			return response.json()['sentiment']['document']['label']
+	except KeyError:
+			return 'neutral'
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-from .restapis import get_dealers_from_cf
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -102,8 +102,17 @@ def get_dealerships(request):
 		return HttpResponse(dealer_names)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+	if request.method == "GET":
+		url_r = f"https://montoyanieve-5000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/get_reviews?id={dealer_id}"
+		url_ds = f"https://montoyanieve-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get?id={dealer_id}"
+		# Get dealers from the URL
+		print(get_dealer_reviews_from_cf(url_r, dealer_id))
+		context = {
+			"dealer": get_dealers_from_cf(url_ds)[0],
+			"reviews": get_dealer_reviews_from_cf(url_r, dealer_id),
+		}
+		return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
